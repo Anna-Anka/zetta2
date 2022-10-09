3a063de5cd3c341f9c26d21c8a373c8f51d2dbd6
Anna-Anka <anjbekrenewa@gmail.com>
2022-10-09 14:33:38 +0300

    corrected some moments

--- src/js/components/_interactive.js
+++ src/js/components/_interactive.js
@@ -5,23 +5,16 @@ export const interactiveFunction = () => {
                 $('.interactive__btn').not($(this).children('.interactive__btn')).removeClass('interactive__btn--active');
                 $('.interactive__content').not($(this).children('.interactive__content')).slideUp(300);
                 $('.interactive__content--active').not($(this).children('.interactive__content--active')).slideUp(200);
+                $('.interactive__item').not($(this)).removeClass('interactive__item--active')
             };
 
-            $(this).children('.interactive__btn').toggleClass('interactive__btn--active').next().slideToggle(300);
+            $(this).children('.interactive__btn').toggleClass('interactive__btn--active');
+            $(this).children('.interactive__content--desktop').slideToggle(300);
+
             $(this).children('.interactive__content--mobile').slideToggle(200);
+            $(this).toggleClass('interactive__item--active');
         });
     };
 };
 
-export const interactiveBoxFunction = () => {
-    if (document.querySelector('.interactive-box')) {
-        $('.interactive__btn').on('click', function (e) {
-            e.preventDefault();
-            $('.interactive__btn').removeClass('interactive__btn--active');
-            $(this).addClass('interactive__btn--active');
-
-            $('.interactive-box__item').removeClass('interactive-box__item--active');
-            $($(this).attr('href')).addClass('interactive-box__item--active');
-        });
-    };
-};
+export const interactiveBoxFunction = () => {};
