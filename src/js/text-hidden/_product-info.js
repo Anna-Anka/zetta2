0bcc08debbea610d4949e9e6d8ff3238770274f2
Anna-Anka <anjbekrenewa@gmail.com>
2022-10-18 21:17:32 +0300

    restored first slider in home page, corrected styles for form in 3d project page, corrected text-hodden in catalog page

--- src/js/text-hidden/_product-info.js
+++ src/js/text-hidden/_product-info.js
@@ -17,21 +17,23 @@ export const textHiddenProductInfo = () => {
 			};
 		});
 
-		const btnDetail = document.querySelector('.product-info__detail');
-		const infoRight = document.querySelector('.product-info__right');
+        if (document.querySelector('.product-info__detail')) {
+            const btnDetail = document.querySelector('.product-info__detail');
+            const infoRight = document.querySelector('.product-info__right');
 
-		btnDetail.addEventListener('click', () => {
-			infoRight.classList.toggle('product-info__right--active');
-		});
+            btnDetail.addEventListener('click', () => {
+                infoRight.classList.toggle('product-info__right--active');
+            });
 
-		document.addEventListener('mousedown', (e) => {
-			if (document.querySelector('.product-info__right--active')) {
-				const hidden = document.querySelector('.product-info__right--active .product-info__hidden');
-				const btn = document.querySelector('.product-info__right--active .product-info__detail');
-				if (!hidden.contains(e.target) && !btn.contains(e.target)) {
-					hidden.closest('.product-info__right').classList.remove('product-info__right--active');
-				}
-			}
-		});
+            document.addEventListener('mousedown', (e) => {
+                if (document.querySelector('.product-info__right--active')) {
+                    const hidden = document.querySelector('.product-info__right--active .product-info__hidden');
+                    const btn = document.querySelector('.product-info__right--active .product-info__detail');
+                    if (!hidden.contains(e.target) && !btn.contains(e.target)) {
+                        hidden.closest('.product-info__right').classList.remove('product-info__right--active');
+                    }
+                }
+            });
+        }
 	};
 };
